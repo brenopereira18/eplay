28ed82b58c037a0cc51c8180b8d4adea708ee884
Breno Pereira <brenopereirabetti@gmail.com>
2024-07-01 15:09:18 -0300

    implenta requisição para a para a página de finalização de compra

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,10 +6,12 @@ import carrinho from '../../assets/images/carrinho.svg'
 import { open } from '../../store/reducers/cart'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootReducer } from '../../store'
+import { useState } from 'react'
 
 const Header = () => {
   const dispatch = useDispatch()
   const { items } = useSelector((state: RootReducer) => state.cart)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const openCart = () => {
     dispatch(open())
@@ -17,28 +19,48 @@ const Header = () => {
 
   return (
     <S.HeaderBar>
-      <div>
-        <Link to="/">
-          <img src={logo} alt="EPLAY" />
-        </Link>
-        <nav>
-          <S.Link>
-            <S.LinkItem>
-              <Link to="/categories">Categorias</Link>
-            </S.LinkItem>
-            <S.LinkItem>
-              <a href="#">Novidades</a>
-            </S.LinkItem>
-            <S.LinkItem>
-              <a href="#">Promoções</a>
-            </S.LinkItem>
-          </S.Link>
-        </nav>
-      </div>
-      <S.CartButton onClick={openCart}>
-        {items.length} - produtos(s)
-        <img src={carrinho} alt="Carrinho de compras" />
-      </S.CartButton>
+      <S.HeaderRow>
+        <div>
+          <S.Hamburguer onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <span />
+            <span />
+            <span />
+          </S.Hamburguer>
+          <Link to="/">
+            <img src={logo} alt="EPLAY" />
+          </Link>
+          <nav>
+            <S.Link>
+              <S.LinkItem>
+                <Link to="/categories">Categorias</Link>
+              </S.LinkItem>
+              <S.LinkItem>
+                <a href="#">Novidades</a>
+              </S.LinkItem>
+              <S.LinkItem>
+                <a href="#">Promoções</a>
+              </S.LinkItem>
+            </S.Link>
+          </nav>
+        </div>
+        <S.CartButton onClick={openCart}>
+          {items.length} <span>- produtos(s)</span>
+          <img src={carrinho} alt="Carrinho de compras" />
+        </S.CartButton>
+      </S.HeaderRow>
+      <S.NavMobile className={isMenuOpen ? 'is-open' : ''}>
+        <S.Link>
+          <S.LinkItem>
+            <Link to="/categories">Categorias</Link>
+          </S.LinkItem>
+          <S.LinkItem>
+            <a href="#">Novidades</a>
+          </S.LinkItem>
+          <S.LinkItem>
+            <a href="#">Promoções</a>
+          </S.LinkItem>
+        </S.Link>
+      </S.NavMobile>
     </S.HeaderBar>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,38 +1,69 @@
 import styled from 'styled-components'
 import { breakpoints, cores } from '../../styles'
 
+export const Link = styled.ul`
+  display: flex;
+  margin-left: 40px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    margin-left: 0;
+    display: block;
+  }
+`
+
 export const HeaderBar = styled.header`
   background-color: ${cores.cinza};
   padding: 24px;
   border-radius: 16px;
   margin-bottom: 80px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
 
   a {
     color: ${cores.branca};
     font-weight: bold;
     text-decoration: none;
   }
+`
 
-  div {
+export const HeaderRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  > div {
     display: flex;
     align-items: center;
-  }
 
-  @media (max-width: ${breakpoints.mobile}) {
-    display: none;
+    @media (max-width: ${breakpoints.tablet}) {
+      flex: 1;
+      justify-content: space-between;
+
+      ${Link} {
+        display: none;
+      }
+    }
   }
 `
 
-export const Link = styled.ul`
-  display: flex;
-  margin-left: 40px;
+export const NavMobile = styled.nav`
+  display: none;
+
+  &.is-open {
+    display: block;
+  }
 `
 
 export const LinkItem = styled.li`
   margin-right: 16px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    margin-right: 0;
+
+    a {
+      padding: 16px 0;
+      display: block;
+      text-align: center;
+    }
+  }
 `
 
 export const CartButton = styled.a`
@@ -40,5 +71,27 @@ export const CartButton = styled.a`
 
   img {
     margin-left: 16px;
+  }
+
+  @media (max-width: ${breakpoints.tablet}) {
+    span {
+      display: none;
+    }
+  }
+`
+
+export const Hamburguer = styled.div`
+  width: 32px;
+
+  span {
+    height: 2px;
+    width: 100%;
+    display: block;
+    background-color: ${cores.branca};
+    margin-bottom: 4px;
+  }
+
+  @media (min-width: ${breakpoints.tablet}) {
+    display: none;
   }
 `
